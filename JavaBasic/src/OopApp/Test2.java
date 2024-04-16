82371818fd8d8b85d44f5d0112ab9c6922f89283
huangjie4 <1364045672@qq.com>
2024-04-16 15:43:41 +0800

    修改222

---
+++ JavaBasic/src/OopApp/Test2.java
@@ -0,0 +1,9 @@
+package OopApp;
+
+public class Test2 {
+    public static void main(String[] args) {
+        User s1 = new User();
+        User s2 = new User();
+        System.out.println(User.number);
+    }
+}
---
+++ JavaBasic/src/OopApp/User.java
@@ -0,0 +1,8 @@
+package OopApp;
+
+public class User {
+    public static int number;
+    public User(){
+        User.number++;
+    }
+}
