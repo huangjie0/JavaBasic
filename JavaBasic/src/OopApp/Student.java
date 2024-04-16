7493c0ab0d875d8ccb4dfad84c94599daf41afc8
huangjie4 <1364045672@qq.com>
2024-04-16 16:01:21 +0800

    修改111

--- JavaBasic/src/OopApp/Student.java
+++ JavaBasic/src/OopApp/Student.java
@@ -1,8 +1,16 @@
 package OopApp;
 
 public class Student {
+    double score;
     //类变量
     static String name;
     //成员变量（对象变量）
     int age;
+    public static void printHelloWorld(){
+        System.out.println("hello world");
+        System.out.println("Hello world");
+    }
+    public void printScore(){
+        System.out.println("成绩：" + (score >= 60 ? "及格" : "不及格"));
+    }
 }
--- JavaBasic/src/OopApp/Test.java
+++ JavaBasic/src/OopApp/Test.java
@@ -7,10 +7,17 @@ public class Test {
         //不推荐
         Student s1 = new Student();
         s1.name="看看模块";
+        s1.score = 90;
 
         //不推荐
         Student s2 = new Student();
         s2.name = "斤斤计较";
         System.out.println(Student.name);
+        Student.printHelloWorld();
+        Student s3 = new Student();
+        s3.printHelloWorld();
+
+        //实例方法使用
+        s1.printScore();
     }
 }
