b81104c0883db6cd7bdb017cafe9a00ae724913f
huangjie4 <1364045672@qq.com>
2024-04-22 15:09:24 +0800

    修改

--- JavaBasic/src/SubclassConstructor/Test.java
+++ JavaBasic/src/SubclassConstructor/Test.java
@@ -7,16 +7,21 @@ public class Test {
     }
 }
 class F{
-    public F(){
-        System.out.println("+++++父类无参数构造器执行了");
+//    public F(){
+//        System.out.println("+++++父类无参数构造器执行了");
+//    }
+    public F(String name,int age){
+
     }
 }
 
 class Z extends F{
     public Z(){
+        super("黄鸡",17);
         System.out.println("=====子类无参数构造器执行了");
     }
     public Z(String name){
+        super("晃晃",18);
         System.out.println("----------子类有参构造器执行了----");
     }
 }
