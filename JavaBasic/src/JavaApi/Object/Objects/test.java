67f538e08129471610953a3370b16e48c2f0ba93
huangjie4 <1364045672@qq.com>
2024-04-24 15:48:27 +0800

    Objects

--- JavaBasic/src/JavaApi/Object/Objects/test.java
+++ JavaBasic/src/JavaApi/Object/Objects/test.java
@@ -1,10 +1,18 @@
 package JavaApi.Object.Objects;
 
+import java.util.Objects;
+
 public class test {
     public static void main(String[] args) {
-        String s1 = "黄杰";
+        String s1 = null;
         String s2 = "黄杰";
-        System.out.println(s1.equals(s2));
+
+        //System.out.println(s1.equals(s2));
+        System.out.println(Objects.equals(s1,s2));
+        System.out.println(Objects.isNull(s1));
+        System.out.println(Objects.isNull(s2));
+        System.out.println(Objects.nonNull(s1));
+        System.out.println(Objects.nonNull(s2));
 
     }
 }
---
+++ JavaBasic/src/PackagingGroup/test.java
@@ -0,0 +1,13 @@
+package PackagingGroup;
+
+public class test {
+    public static void main(String[] args) {
+        //包装类
+//        Integer a = new Integer(12);
+        Integer a2 = Integer.valueOf(12);
+        System.out.println(a2);
+
+        //自动将基本数据类型转化为对象
+
+    }
+}
