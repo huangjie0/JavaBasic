0e23e96eb3b92848150e1321e48a568795fba2f6
huangjie4 <1364045672@qq.com>
2024-04-25 15:02:11 +0800

    JDK8相关时间操作

--- JavaBasic/src/JDK8Time/LocalDate_Test.java
+++ JavaBasic/src/JDK8Time/LocalDate_Test.java
@@ -26,7 +26,22 @@ public class LocalDate_Test {
         //修改年月日相关信息,基本用with
         LocalDate ld2 = ld.withYear(2099);
         LocalDate ld3 = ld.withMonth(5);
+        LocalDate ld4 = ld.plusYears(2);
+        LocalDate ld5 = ld.plusDays(3);
+        LocalDate ld6 = ld.plusMonths(1);
         System.out.println(ld2);
         System.out.println(ld3);
+
+
+        //获取指定的localDate对象
+        LocalDate ld22 = LocalDate.of(2099,12,12);
+        LocalDate ld29 = LocalDate.of(2011,12,12);
+
+        //判断2个日期对象是否相等，在前还是在后
+        System.out.println( ld22.equals(ld29));
+        //之后
+        System.out.println(ld22.isAfter(ld29));
+        //之前
+        System.out.println(ld22.isBefore(ld29));
     }
 }
