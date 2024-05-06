1b0ae029db5a9de7b898f7f69f02ee1eb5f8351a
huangjie4 <1364045672@qq.com>
2024-05-06 14:26:52 +0800

    hash

--- JavaBasic/src/SetProgression/SetTest.java
+++ JavaBasic/src/SetProgression/SetTest.java
@@ -18,6 +18,15 @@ public class SetTest {
         s.add(6);
         s.add(6);
         System.out.println(s);
+        Student s1 = new Student("黄杰",18,23.5);
+        Student s2 = new Student("小明",20,24.5);
+        Student s3 = new Student("小海",22,22.5);
+        Student s4 = new Student("小李",29,20.5);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
+        System.out.println(s4.hashCode());
+        //哈希表
 
     }
 }
