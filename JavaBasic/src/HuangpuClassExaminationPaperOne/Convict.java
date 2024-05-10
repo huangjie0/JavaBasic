849df0d649cabe227bba0dd25ed70ae6d1084b6b
huangjie4 <1364045672@qq.com>
2024-05-10 09:35:59 +0800

    黄埔班试卷一囚犯幸存者案例

---
+++ JavaBasic/src/HuangpuClassExaminationPaperOne/Convict.java
@@ -0,0 +1,33 @@
+package HuangpuClassExaminationPaperOne;
+/**
+ * 囚犯对象
+ * */
+
+public class Convict {
+    private int code;    //囚犯编号
+    private int location;    //囚犯位置
+
+    public Convict(int code, int location) {
+        this.code = code;
+        this.location = location;
+    }
+
+    public Convict() {
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+
+    public void setLocation(int location) {
+        this.location = location;
+    }
+}
---
+++ JavaBasic/src/HuangpuClassExaminationPaperOne/Demo1.java
@@ -0,0 +1,8 @@
+package HuangpuClassExaminationPaperOne;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        //完成幸存者案例，每个囚犯是一个对象来实现
+        //创建100个囚犯，依次占位，并赋值（1-200的数量）
+    }
+}
