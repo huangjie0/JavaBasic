b1cf4ec941de99717b4056b2478cef94deee5035
huangjie4 <1364045672@qq.com>
2024-05-11 17:08:28 +0800

    io流

--- JavaBasic/src/Io/Test.java
+++ JavaBasic/src/Io/Test.java
@@ -1,9 +1,6 @@
 package Io;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 
 public class Test {
     public static void main(String[] args) throws Exception {
@@ -11,7 +8,8 @@ public class Test {
         //字节输入流 抽象类   inputStream  字节输出流 outputStream  字符输入流 Reader  字符输出流 Writer
         //实现类 FileInputStream FileOutPutStream   FileReader FileWriter
 //        InputStream f = new FileInputStream(new File("JavaBasic\\src\\aa.txt"));
-        InputStream f = new FileInputStream("src\\aa.txt");
+//        InputStream f = new FileInputStream("src\\aa.txt");
+        OutputStream f = new FileOutputStream("src\\bb.txt");
         //开始读取字节数据,每次读取一个字节,返回
 //        int b = f.read();
 //        System.out.println((char)b);
@@ -56,8 +54,19 @@ public class Test {
 //        System.out.println(len);
 
         //一次性读取全部字节优化操作
-        byte[] buffer = f.readAllBytes();
-        System.out.println(new String(buffer));
+//        byte[] buffer = f.readAllBytes();
+//        System.out.println(new String(buffer));
 
+        //字节输出流
+        f.write(97); //97是一个字节代表a
+        f.write('b'); // 'b' 98
+//        f.write('黄');
+
+        byte[] bytes = "我爱你中国".getBytes();
+        f.write(bytes);
+
+        f.write(bytes,0,15);
+
+        f.close();
     }
 }
