7c1c862301243e56ab5493afd00e6e0e612eac26
huangjie4 <1364045672@qq.com>
2024-05-11 10:44:49 +0800

    File文件案例递归操作

---
+++ JavaBasic/src/Recursion/Test.java
@@ -0,0 +1,22 @@
+package Recursion;
+/**
+ * 方法递归
+ * */
+
+public class Test {
+    public static void main(String[] args) {
+        test1();
+    }
+    public static void test1(){
+        System.out.println(111);
+        test1();
+    }
+    public static void test2(){
+        System.out.println("---test2---");
+        test3();
+    }
+    public static void test3(){
+        test2();
+    }
+
+}
