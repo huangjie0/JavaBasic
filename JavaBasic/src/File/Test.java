9414194595fc2e1ccb1bff220c438514f44595e4
huangjie4 <1364045672@qq.com>
2024-05-11 09:12:23 +0800

    File文件的操作方法

--- JavaBasic/src/File/Test.java
+++ JavaBasic/src/File/Test.java
@@ -1,11 +1,12 @@
 package File;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 
 public class Test {
     public static void main(String[] args) {
-//        File f1 = new File("F:\\ps\\111.png");
-        File f1 = new File("F:" + File.separator + "ps" + File.separator + "111.png");
+        File f1 = new File("F:\\ps\\ab.txt");
+//        File f1 = new File("F:" + File.separator + "ps" + File.separator + "111.png");
         //返回文件字节个数,文件大小
         System.out.println(f1.length());
         System.out.println(f1.exists());
@@ -14,6 +15,18 @@ public class Test {
         System.out.println(f2.length());
         //判断文件存不存在
         System.out.println(f2.exists());
+        //判断是否是文件，file
+        System.out.println(f1.isFile());
+        //判断是否是文件夹
+        System.out.println(f1.isDirectory());
+        //获取文件的名字
+        System.out.println(f1.getName());
+        //返回文件的字节个数
+        System.out.println(f1.length());
+        //获取文件的最后修改时间
+//        f1.lastModified()
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        System.out.println(sdf.format(f1.lastModified()));
 
     }
 }
--- JavaBasic/src/File/fileTest.java
+++ JavaBasic/src/File/fileTest.java
@@ -6,7 +6,5 @@ public class fileTest {
     public static void main(String[] args) {
         File f = new File("F:\\ps\\ab.txt");
         System.out.println(f.length());
-
-
     }
 }
