5c9ac21d88f625f2c27b074cf1584d3aa7231e2f
huangjie4 <1364045672@qq.com>
2024-05-13 14:09:47 +0800

    文件字符读取操作

--- JavaBasic/src/FileWriterTest/Test.java
+++ JavaBasic/src/FileWriterTest/Test.java
@@ -5,7 +5,7 @@ import java.io.Writer;
 
 public class Test {
     public static void main(String[] args) {
-        try (Writer r = new FileWriter("src/aaa.txt")){
+        try (Writer r = new FileWriter("src/aaa.txt",true)){
             r.write('a');
             r.write(97);
             r.write('黄');
@@ -21,5 +21,4 @@ public class Test {
             e.printStackTrace();
         }
     }
-
 }
