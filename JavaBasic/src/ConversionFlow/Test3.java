65c89451e65b955fee99fe3e02854017a7082488
huangjie4 <1364045672@qq.com>
2024-05-13 17:11:13 +0800

    字符输出转换流使用

--- JavaBasic/src/ConversionFlow/Test3.java
+++ JavaBasic/src/ConversionFlow/Test3.java
@@ -3,9 +3,20 @@ package ConversionFlow;
 * 字符输出转换流
 * */
 
+import java.io.*;
+
 public class Test3 {
     public static void main(String[] args) {
         //控制写出的字符编码，写出文件
-
+        try(
+                OutputStream os = new FileOutputStream("src/m.txt");
+                Writer w = new OutputStreamWriter(os,"UTF-8");
+                BufferedWriter bw = new BufferedWriter(w);
+            ) {
+            bw.write("我是中国人");
+            bw.write("我爱你中国");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
