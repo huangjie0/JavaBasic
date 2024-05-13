a0b5babffb54f0364f53f57b62873718765d7f7f
huangjie4 <1364045672@qq.com>
2024-05-13 14:07:30 +0800

    文件字符读取操作

--- JavaBasic/src/FileWriterTest/Test.java
+++ JavaBasic/src/FileWriterTest/Test.java
@@ -9,10 +9,14 @@ public class Test {
             r.write('a');
             r.write(97);
             r.write('黄');
+            r.write("\r\n");
             //写入字符串进去
             r.write("我爱你重化工");
             //写入字符串一部分
             r.write("我爱你中国",0,2);
+            //写一个字符数组进去
+            char[] buffer = {'a','b','c','i','马'};
+            r.write(buffer,0,1);
         } catch (Exception e) {
             e.printStackTrace();
         }
