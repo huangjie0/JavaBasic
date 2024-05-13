4f19cccd9bf8ef4566ed743ad9e34da436b98115
huangjie4 <1364045672@qq.com>
2024-05-13 13:44:09 +0800

    文件字符读取操作

--- JavaBasic/src/FileReader/Test.java
+++ JavaBasic/src/FileReader/Test.java
@@ -10,9 +10,15 @@ public class Test {
     public static void main(String[] args){
         try (Reader r = new FileReader("E:src\\aa.txt")) {
             //开始读取内容
-            int c; //记住每次读取的字符编号
-            while ((c = r.read()) != -1){
-                System.out.println((char)c);
+//            int c; //记住每次读取的字符编号
+//            while ((c = r.read()) != -1){
+//                System.out.println((char)c);
+//            }
+            //每次读取多个字符
+            char[] buffer= new char[3];
+            int len;
+            while ((len=r.read(buffer)) != -1){
+                System.out.print(new String(buffer, 0, len));
             }
         }catch (Exception e){
             e.printStackTrace();
