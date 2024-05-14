1873b22cf8d0e377f56debbce5b293f90bf82a3f
huangjie4 <1364045672@qq.com>
2024-05-14 15:04:13 +0800

    打印流操作

--- JavaBasic/src/PrintStream/Test.java
+++ JavaBasic/src/PrintStream/Test.java
@@ -1,12 +1,23 @@
 package PrintStream;
+
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+
 /**
- *
  * 打印流
  * */
 
 public class Test {
     public static void main(String[] args) {
         //printStream printWriter  打印输入流 打印输出流
-
+        try (PrintStream ps = new PrintStream("src/ln.txt", Charset.forName("UTF-8"));){
+            ps.println(97);
+            ps.println('a');
+            ps.print("你好哦");
+            ps.print(true);
+            ps.print(99.5);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
