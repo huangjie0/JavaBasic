d6853ca9bb9cb9e03f55284787b8a9615d073b2f
huangjie4 <1364045672@qq.com>
2024-05-15 17:22:33 +0800

    线程的一些方法获取线程的名字设置名字等等

--- JavaBasic/src/Multithreading/MyThread.java
+++ JavaBasic/src/Multithreading/MyThread.java
@@ -6,9 +6,10 @@ package Multithreading;
 public class MyThread  extends  Thread{
     @Override
     public void run(){
+        Thread t = Thread.currentThread();
         //描述线程执行顺序
         for (int i = 0; i <= 5; i++) {
-            System.out.println("序号" + i);
+            System.out.println(t.getName() + "输出" + i);
         }
     }
 }
---
+++ JavaBasic/src/Multithreading/Test5.java
@@ -0,0 +1,28 @@
+package Multithreading;
+/**
+ * 线程的一些方法
+ * */
+
+public class Test5 {
+    public static void main(String[] args) {
+        Thread t1 = new MyThread();
+        t1.setName("1号子线程");
+        t1.start();
+        System.out.println(t1.getName());
+
+        Thread t2 = new MyThread();
+        t2.setName("2号子线程");
+        t2.start();
+        System.out.println(t2.getName());
+
+        //主线程的对象
+        //哪个线程执行就会得到哪个线程的名字
+        Thread m = Thread.currentThread();
+        System.out.println(m.getName());  //main
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println("main线程输出" + i);
+        }
+
+    }
+}
