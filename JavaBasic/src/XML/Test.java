c394658870f5be807df3c8d3d6c19147eb9b7484
huangjie4 <1364045672@qq.com>
2024-05-15 11:27:04 +0800

    使用dom4j框架读取xml文件

--- JavaBasic/src/XML/Test.java
+++ JavaBasic/src/XML/Test.java
@@ -7,6 +7,8 @@ import org.dom4j.Document;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) throws Exception {
         //创建dom4j解析器对象
@@ -15,7 +17,21 @@ public class Test {
         Document d = saxReader.read("src\\XML\\huangjie.xml");
         //获取根元素对象
         Element root = d.getRootElement();
-        System.out.println(root.getName());
+//        System.out.println(root.getName());
+
+        //获取根元素下面的全部一级子元素
+        List<Element> elements = root.elements();
+        for (Element element : elements) {
+            System.out.println(element.getName());
+        }
+        //获取指定元素的
+        List<Element> elements2 = root.elements("User");
+        for (Element element : elements2) {
+            System.out.println(element.getName());
+        }
+        //获取当前元素下的某个元素
+        Element p = root.element("People");
+        System.out.println(p.getName());
 
     }
 }
