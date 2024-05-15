dd385bf4a37d99cb752a779e3a55960da737e224
huangjie4 <1364045672@qq.com>
2024-05-15 17:06:19 +0800

    线程三匿名内部类简化写法lambda

--- JavaBasic/src/Multithreading/MyThread3.java
+++ JavaBasic/src/Multithreading/MyThread3.java
@@ -1,8 +1,26 @@
 package Multithreading;
+
+import java.util.concurrent.Callable;
+
 /**
  * 使用callable创建多线程
  * */
 
-public class MyThread3 {
+public class MyThread3 implements Callable<String> {
+    private int n;
 
+    public MyThread3(int n) {
+        this.n = n;
+    }
+
+    //重写call方法
+    @Override
+    public String call() throws Exception {
+//        描述线程的任务并返回结果
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum+=i;
+        }
+        return "求出了1-" + n + "的和为" + sum;
+    }
 }
--- JavaBasic/src/Multithreading/Test4.java
+++ JavaBasic/src/Multithreading/Test4.java
@@ -1,7 +1,19 @@
 package Multithreading;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 public class Test4 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Callable<String> call = new MyThread3(200);
+        //把callable封装成futureTask对象，任务对象
+
+        FutureTask<String> f = new FutureTask<>(call);
+        new Thread(f).start();
+
+        //获取线程完毕的结果
+        String s = f.get();
+        System.out.println(s);
 
     }
 }
