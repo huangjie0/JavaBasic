0684d95ff849b32e5861cf8b7238a232921851be
huangjie4 <1364045672@qq.com>
2024-05-15 10:56:20 +0800

    使用dom4j框架读取xml文件

--- JavaBasic/src/XML/Test.java
+++ JavaBasic/src/XML/Test.java
@@ -1,7 +1,21 @@
 package XML;
+/*
+* 解析xml文件
+* */
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        //创建dom4j解析器对象
+        SAXReader saxReader = new SAXReader();
+        //读取文档对象
+        Document d = saxReader.read("src\\XML\\huangjie.xml");
+        //获取根元素对象
+        Element root = d.getRootElement();
+        System.out.println(root.getName());
 
     }
 }
