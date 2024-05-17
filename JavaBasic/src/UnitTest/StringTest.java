1e7451dbe754215529439a9ab9986442d29dd5bc
huangjie4 <1364045672@qq.com>
2024-05-17 17:31:03 +0800

    测试的注解了解

--- JavaBasic/src/UnitTest/StringTest.java
+++ JavaBasic/src/UnitTest/StringTest.java
@@ -1,9 +1,18 @@
 package UnitTest;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 
 public class StringTest {
+    @Before
+    public void test1(){
+        System.out.println("------>test1在之前就执行了！");
+    }
+
+    @After
+    public void test2(){
+        System.out.println("------->after方法执行完成！");
+    }
+
     @Test
     public void testPrintNumber(){
         StringUtil.printNumber("huangjie");
@@ -19,7 +28,16 @@ public class StringTest {
         System.out.println(index2);
 
         //断言机制，可以预测业务方法结果
-        Assert.assertEquals("方法内部有bug",4,index2);
+//        Assert.assertEquals("方法内部有bug",4,index2);
+    }
 
+    @AfterClass
+    public static void test11(){
+        System.out.println("------->AfterClass方法执行了！");
+    }
+
+    @BeforeClass
+    public static void test112(){
+        System.out.println("------->BeforeClass方法执行了！");
     }
 }
