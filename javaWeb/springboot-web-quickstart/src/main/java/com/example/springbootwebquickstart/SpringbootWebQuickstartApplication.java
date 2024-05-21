3c239d4d707fc8e1122b44851f8fa82f5b1209bc
huangjie4 <1364045672@qq.com>
2024-05-21 13:37:01 +0800

    修改

---
+++ javaWeb/springboot-web-quickstart/src/main/java/com/example/springbootwebquickstart/SpringbootWebQuickstartApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootwebquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//启动类
+@SpringBootApplication
+public class SpringbootWebQuickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebQuickstartApplication.class, args);
+    }
+}
