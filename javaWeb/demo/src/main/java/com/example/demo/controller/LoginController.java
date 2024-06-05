91cb46d5d19c8bf33ffb8e65b5503773fc822f67
huangjie4 <1364045672@qq.com>
2024-06-05 09:07:58 +0800

    spring-boot的jwt加解

--- javaWeb/demo/src/main/java/com/example/demo/controller/LoginController.java
+++ javaWeb/demo/src/main/java/com/example/demo/controller/LoginController.java
@@ -3,11 +3,15 @@ package com.example.demo.controller;
 import com.example.demo.pojo.Emp;
 import com.example.demo.pojo.Result;
 import com.example.demo.service.EmpService;
+import com.example.demo.utils.JwtUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -19,6 +23,17 @@ public class LoginController {
     public Result login(@RequestBody Emp emp){
         log.info("员工登录");
         Emp emp1 = empService.login(emp);
-        return emp1 != null?Result.success():Result.error("用户名或密码错误！");
+
+        //登录成功，生成令牌，下发令牌
+        if(emp1 != null) {
+            Map<String,Object> claims = new HashMap<>();
+            claims.put("id",emp1.getId());
+            claims.put("name",emp1.getName());
+            claims.put("username",emp1.getUsername());
+
+            String jwt = JwtUtils.generateJwt(claims); //包含员工登录信息
+            return Result.success(jwt);
+        }
+        return Result.error("用户名或密码错误！");
     }
 }
---
+++ javaWeb/demo/src/main/java/com/example/demo/utils/JwtUtils.java
@@ -0,0 +1,40 @@
+package com.example.demo.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.Map;
+
+public class JwtUtils {
+    private static String signKey = "itheima";//签名密钥
+    private static Long expire = 43200000L; //有效时间
+
+    /**
+     * 生成JWT令牌
+     * @param claims JWT第二部分负载 payload 中存储的内容
+     * @return
+     */
+    public static String generateJwt(Map<String, Object> claims){
+        String jwt = Jwts.builder()
+                .addClaims(claims)//自定义信息（有效载荷）
+                .signWith(SignatureAlgorithm.HS256, signKey)//签名算法（头部）
+                .setExpiration(new Date(System.currentTimeMillis() + expire))//过期时间
+                .compact();
+        return jwt;
+    }
+
+    /**
+     * 解析JWT令牌
+     * @param jwt JWT令牌
+     * @return JWT第二部分负载 payload 中存储的内容
+     */
+    public static Claims parseJWT(String jwt){
+        Claims claims = Jwts.parser()
+                .setSigningKey(signKey)//指定签名密钥
+                .parseClaimsJws(jwt)//指定令牌Token
+                .getBody();
+        return claims;
+    }
+}
